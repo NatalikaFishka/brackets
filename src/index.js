baea2d51235ed70e55d6bc1c293c42aa56ba9ded
Natalya Okuneva <okuneva_natasha@mail.ru>
2019-09-26 18:23:57 +0300

    feat: count brackes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let newStr = str[0];
+    str = str.substr(1);
+
+    while (str.length > 0) {
+
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            if (str[0] === bracketsConfig[i][1] && newStr[newStr.length - 1] === bracketsConfig[i][0]) {
+                str = str.substr(1);
+                newStr = newStr.substr(0, (newStr.length - 1));
+            } else if (str[0] === bracketsConfig[i][0]) {
+                newStr += str[0];
+                str = str.substr(1);
+            } else if (str[0] === bracketsConfig[i][1] && newStr[newStr.length - 1] !== bracketsConfig[i][0]) {
+                return false;
+            }
+
+        }
+
+
+    }
+
+    return !newStr.length;;
 }
